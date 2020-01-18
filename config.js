c0def1802e4225ca2a647ea58ade3382f189ea4b
seunggabi <seunggabi.kim@navercorp.com>
2020-01-18 21:46:31 +0900

    feat(#1): multiple page set refresh

--- config.js
+++ config.js
@@ -1,9 +1,1 @@
-/**
- * Created by seunggabi on 2017. 8. 9..
- */
-
-var MASTER_URL = "http://seunggabi.com";
-var URL = "";
-var COUNT = "";
-
-var DATAS = ["url", "count", "period"];
+const KEYS = ["url", "count", "period"];
--- popup_script.js
+++ popup_script.js
@@ -1,64 +1,56 @@
 function save() {
-	DATAS.forEach(function(item) {
-		var value = document.querySelector("#"+item).value;
+    let url = document.querySelector("#url").value;
 
-		chrome.cookies.set({
-			url: MASTER_URL,
-			path: "/",
-			name: item,
-			value: value,
-			expirationDate: makeDateTime(365)
-		});
-
-		if(item === "url") {
-			let protocol = value;
-			DATAS.forEach(function(key) {
-				chrome.cookies.set({
-					url: protocol,
-					path: "/",
-					name: key,
-					value: document.querySelector("#"+key).value,
-					expirationDate: makeDateTime(365)
-				});
-			});
-		}
-	});
+    KEYS.forEach(function(name) {
+        let { value } = document.querySelector("#"+name);
+        chrome.cookies.set({
+            url,
+            path: "/",
+            name,
+            value,
+            expirationDate: makeDateTime(365)
+        });
+    });
 }
 
 function toggleSiteList() {
-	if(document.querySelector("#siteList").className.indexOf("hidden") >= 0) {
-		document.querySelector("#siteList").className = "";
-	} else {
-		document.querySelector("#siteList").className = "hidden";
-	}
+    if(document.querySelector("#siteList").className.indexOf("hidden") >= 0) {
+        document.querySelector("#siteList").className = "";
+    } else {
+        document.querySelector("#siteList").className = "hidden";
+    }
 }
 
 function renderView() {
-	DATAS.forEach(function(item) {
-		chrome.cookies.get({url:MASTER_URL, name:item}, function(c) {
-			var DOM = document.querySelector("#"+item);
-			DOM.value = c.value;
-		});
-	});
+
+    KEYS.forEach(function(name) {
+        chrome.tabs.getSelected(null, function(tab) {
+            chrome.cookies.get({url: tab.url, name}, function(c) {
+                if(!c) return;
+
+                document.querySelector("#"+name).value = c.value;
+            });
+        });
+    });
 }
 
 function bindEvent() {
-	var siteListButton = document.querySelector("#siteListButton");
-	siteListButton && siteListButton.addEventListener("click", toggleSiteList);
+    const siteListButton = document.querySelector("#siteListButton");
+    siteListButton && siteListButton.addEventListener("click", toggleSiteList);
 
-	var saveButton = document.querySelector("button#save");
-	saveButton && saveButton.addEventListener("click", save);
+    const saveButton = document.querySelector("button#save");
+    saveButton && saveButton.addEventListener("click", save);
 }
 
 function init() {
-	renderView();
-	bindEvent();
+    renderView();
+    bindEvent();
 }
 
 function makeDateTime(exdays) {
-	var d = new Date();
-	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-	return d.getTime();
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    return d.getTime();
 }
 
 window.addEventListener("load", init);
